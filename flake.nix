8ff2d71313503deb2356c1293e7a26198cbe7468
Mistivia <i@mistivia.com>
2026-03-14 15:42:22 +0800

    add nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "accessor-hs - A Haskell accessor library";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskellPackages;
+
+        package = haskellPackages.callCabal2nix "accessor-hs" ./. {};
+      in
+      {
+        packages = {
+          default = package;
+          accessor-hs = package;
+        };
+
+        devShells.default = haskellPackages.shellFor {
+          packages = p: [ package ];
+          withHoogle = true;
+          buildInputs = with haskellPackages; [
+            cabal-install
+            ghcid
+            haskell-language-server
+            hlint
+            ormolu
+          ];
+        };
+      });
+}
